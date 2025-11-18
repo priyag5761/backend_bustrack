fb868ac74006cd34a59ea1cc813f23a65acab6e8
priyag5761 <priyanka30513@gmail.com>
2025-11-18 19:16:37 +0530

    Add files via upload

---
+++ config/mongo.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export async function connectMongo() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ Connected to MongoDB");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err.message);
+  }
+}
